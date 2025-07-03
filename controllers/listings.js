02cb9c783a9c5e414e3ef7b5ba265dad291cb61b
Akash-75-76 <deshmukhaakash752005@gmail.com>
2025-07-03 12:21:31 +0530

    Solved error

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,3 +1,7 @@
+const { listingSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+
+
 const Listing = require("../models/listing");
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -28,12 +32,15 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let url=req.file.path;
+  let filename=req.file.filename;
   let result = listingSchema.validate(req.body);
   if (result.error) {
     throw new ExpressError(400, result.error);
   }
   const newListings = new Listing(req.body.listing);
   newListings.owner = req.user._id;
+  newListings.image={url,filename};
   await newListings.save();
   req.flash("success", "New Listing created");
   res.redirect("/listings");
