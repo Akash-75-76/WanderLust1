b8d9733c53b77344ba04be9b13bce08bb850291a
Akash-75-76 <deshmukhaakash752005@gmail.com>
2025-07-03 12:21:08 +0530

    Updated the image schema

--- models/listing.js
+++ models/listing.js
@@ -9,16 +9,8 @@ const listeningSchema = new Schema({
   },
   description: String,
   image: {
-    filename: String,
-    url: {
-      type: String,
-      default:
-        "https://plus.unsplash.com/premium_photo-1673002094195-f18084be89ce?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      set: (v) =>
-        v === ""
-          ? "https://plus.unsplash.com/premium_photo-1673002094195-f18084be89ce?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          : v,
-    },
+   url:String,
+   filename:String,
   },
   price: Number,
   location: String,
